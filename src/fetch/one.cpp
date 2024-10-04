099d24dfe8cfa8a340da33ca67f6be33f6f6d30a
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-10-04 09:54:49 +0000

    Figuring out clang format

--- src/fetch/one.cpp
+++ src/fetch/one.cpp
@@ -7,48 +7,38 @@
 #include "internal/one.hh"
 #include <filesystem>
 
-void one::fetch_one(httplib::Server& svr) {
-                svr.Post("/", [&](const httplib::Request& req, httplib::Response& res) {
-                        // cout << req.body << "\n";
-                        data_one = nlohmann::json::parse(req.body);
-                        res.status = 200;
-                        return true;
-                });
-                svr.listen("0.0.0.0", 10043);
-        }
+void one::fetch_one(httplib::Server &svr) {
+    svr.Post("/", [&](const httplib::Request &req, httplib::Response &res) {
+        // cout << req.body << "\n";
+        data_one = nlohmann::json::parse(req.body);
+        res.status = 200;
+        return true;
+    });
+    svr.listen("0.0.0.0", 10043);
+}
 
 bool one::create_directory() {
-        namespace fs = std::filesystem;
-  
-        fs::path cwd = fs::current_path();
-        fs::path new_dir = cwd / contest_id / id_char;
+    namespace fs = std::filesystem;
+    fs::path cwd = fs::current_path();
+    fs::path new_dir = cwd / contest_id / id_char;
 
-	if (fs::exists(new_dir)) {
-	  return false;
-	}
+    if(fs::exists(new_dir)) {
+        return false;
+    }
+    try {
+        fs::create_directory(new_dir);
+    } catch(const fs::filesystem_error &e) { return false; }
 
-	try {
-	  fs::create_directory(new_dir);
-	} catch (const fs::filesystem_error& e) {
-	  return false;
-	}
-  
-        return true;
+    return true;
 }
 
 bool one::place_files() {
-        // Call write_test_case("in");
-        // Call write_test_case("out");
-        // Call copy_template();
-        return true;
+    // Call write_test_case("in");
+    // Call write_test_case("out");
+    // Call copy_template();
+    return true;
 }
 
-bool one::write_test_case(std::string_view file_name) {
-        
-        return true;
-}
+bool one::write_test_case(std::string_view file_name) { return true; }
 
-bool one::copy_template() {
-
-        return true;
-}
+bool one::copy_template() { return true; }
